d26a0d4bce4b89416fa178c23b91e6ba051754eb
jsdie099 <julianopaulo.santos@hotmail.com>
2020-07-04 13:52:46 -0300

    fazendo o primeiro hello world com graphql

--- src/server.js
+++ src/server.js
@@ -1,7 +1,38 @@
 const express = require("express");
+const grapqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
 const app = express();
-const routes = express.Router();
 
-routes.get("/",(req,res)=>{res.send("Hello")})
-app.use(routes);
+const object = [
+    {name:"juliano",last_name:"Santos"},
+    {name:"andre",last_name:"sant'anna"},
+    {name:"murilo",last_name:"Santos"},
+    {name:"larissa",last_name:"Silveira"},
+];
+
+const schema = buildSchema(`
+    type Query {hello : String}
+`);
+const root = {hello : ()=>'Hello World!'};
+
+
+app.use("/",grapqlHTTP({
+    schema: schema,
+    rootValue: root, 
+    graphiql: true
+}));
+
 app.listen(3331);
+
+
+/*
+USANDO O GRAPHQL SEM O EXPRESS
+const { graphql, buildSchema } = require("graphql"); 
+const schema = buildSchema('type Query {hello : String}');
+
+const root = {hello : ()=>'Hello World!'};
+
+graphql(schema, '{hello}',root).then((response)=>{
+    console.log(response)
+});
+ */
